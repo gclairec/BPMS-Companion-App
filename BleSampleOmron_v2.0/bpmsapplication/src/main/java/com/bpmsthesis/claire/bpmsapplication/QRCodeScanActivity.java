26a020d32830533f2af0e5e2f10bb4b294a54322
GClaireC <itsmegillianc@gmail.com>
2018-04-13 16:11:02 +0800

    Parsed JSONData(PatientID and Frequency) from QR

--- BleSampleOmron_v2.0/bpmsapplication/src/main/java/com/bpmsthesis/claire/bpmsapplication/QRCodeScanActivity.java
+++ BleSampleOmron_v2.0/bpmsapplication/src/main/java/com/bpmsthesis/claire/bpmsapplication/QRCodeScanActivity.java
@@ -1,18 +1,24 @@
 package com.bpmsthesis.claire.bpmsapplication;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class QRCodeScanActivity extends AppCompatActivity {
+    public String patientID;
 
     private Button button;
 
@@ -38,6 +44,13 @@ public class QRCodeScanActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String qrData;
+        String dataQR = null;
+        TextView patID, freq;
+
+        int hours;
+        int minutes;
+        int seconds;
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if(result != null) {
             if(result.getContents() == null) {
@@ -45,7 +58,45 @@ public class QRCodeScanActivity extends AppCompatActivity {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             } else {
                 Log.d("MainActivity", "Scanned");
-                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+//                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                qrData = result.getContents();
+                Toast.makeText(this, "Scanned: " + qrData, Toast.LENGTH_LONG).show();
+
+                patID = (TextView) findViewById(R.id.patientID);
+                freq = (TextView) findViewById(R.id.frequency);
+
+                try {
+                    // get JSONObject from JSON file
+                    JSONObject obj = new JSONObject(qrData);
+                    // fetch JSONObject named employee
+//                    JSONObject employee = obj.getJSONObject("employee");
+                    // get employee name and salary
+                    patientID = obj.getString("patientID");
+//                    salary = obj.getString("salary");
+                    JSONObject freq_qr = obj.getJSONObject("frequency");
+                    hours = freq_qr.getInt("hours");
+                    minutes = freq_qr.getInt("minutes");
+                    seconds = freq_qr.getInt("seconds");
+
+                    try {
+                        SQLiteDatabase mydatabase = openOrCreateDatabase(patientID,MODE_PRIVATE,null);
+                        Toast.makeText(this, "DB Created", Toast.LENGTH_SHORT).show();
+                    }finally {
+
+                    }
+
+                    // set employee name and salary in TextView's
+                    patID.setText("PatientID: "+patientID);
+                    freq.setText(String.format("Frequency: H:%s, M:%s, S:%s", hours, minutes, seconds));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+
+
+
             }
         } else {
             // This is important, otherwise the result will not be passed to the fragment
